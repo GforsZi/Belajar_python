9c113a98ed7f28f35c06f58faffcd36773e4646a
GforsZi <givaldigumelarsetiawan@gmail.com>
2024-12-02 17:38:33 +0700

    Menambahkan percabangan

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
 name = ["Givaldi", "Andi", "Dion"]
 umur = 5 + 7
 print("Hello " + name[1] + " Anda berumur", umur, "tahun")
+if umur > 10:
+  print(name[1] + " ternyata anda sudah besar")
+else:
+  print(name[1] + " anda berjiwa muda")
