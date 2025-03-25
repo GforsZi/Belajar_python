13060af56fda1013b478b620298a3e76bca57ad5
Givaldi Gumelar Setiawan <givaldigumelarsetiawan@gmail.com>
2025-03-25 16:29:10 +0800

    sudah mempelajari beberapa data type

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+import sys
+
+print(sys.version)
+
 name = ["Givaldi", "Andi", "Dion"]
 umur = 5 + 7
 print("Hello " + name[1] + " Anda berumur", umur, "tahun")
@@ -5,3 +9,24 @@ if umur > 10:
   print(name[1] + " ternyata anda sudah besar")
 else:
   print(name[1] + " anda berjiwa muda")
+
+listPerson = [
+  {
+  "name": "Gival",
+  "cash": 1000,
+  "cool": True
+  },
+  {
+  "name": "chino",
+  "cash": 1500,
+  "cool": True
+  },
+]
+print(listPerson[0]["name"])
+
+phone_name, phone_ram, phone_lcd = "redmi", "grey", "amoled"
+player1, player2, player3 = name
+
+print("Hallo nama saya " + player2 + " dan saya memiliki hp "
+ + phone_name +  " dengan ram " + phone_ram + " ditambah layanya juga " + phone_lcd)
+
